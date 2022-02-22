c547e3862d4a095eae8b7ecfe0632ce41555e532
Lyudmila <milo4kamoya@gmail.com>
2022-02-21 16:48:53 -0800

    all questions separated to another file

---
+++ allQuestions.js
@@ -0,0 +1,162 @@
+const allQuestions = [
+  {
+    question: 'How many legs does a spider have??',
+    answer: 'Nine',
+    answer2: 'Eight',
+    answer3: 'Ten',
+    answer4: 'Six',
+    rightAnswer: 'eight'
+  },
+  {
+    question: 'What is the name of the toy cowboy in Toy Story?',
+    answer: 'Woody',
+    answer2: 'Andy',
+    answer3: 'Buzz Lightyear',
+    answer4: 'Sandy',
+    rightAnswer: 'woody'
+  },
+  {
+    question: 'What colors are the stars on the American flag?',
+    answer: 'Gold',
+    answer2: 'Red',
+    answer3: 'Blue',
+    answer4: 'White',
+    rightAnswer: 'white'
+  },
+  {
+    question: 'How many planets are in our solar system?',
+    answer: 'Nine',
+    answer2: 'Eleven',
+    answer3: 'Eight',
+    answer4: 'Ten',
+    rightAnswer: 'eight'
+  },
+  {
+    question: 'Where does Santa Claus live?',
+    answer: 'The South Pole',
+    answer2: 'The North Pole',
+    answer3: 'Antarctica',
+    answer4: 'Alaska',
+    rightAnswer: 'the north pole'
+  },
+  {
+    question: 'What do caterpillars turn into?',
+    answer: 'Dragonfly',
+    answer2: 'Birds',
+    answer3: 'Grasshoppers',
+    answer4: 'Butterflies',
+    rightAnswer: 'butterflies'
+  },
+  {
+    question: 'How many pairs of wings do bees have?',
+    answer: 'One',
+    answer2: 'Two',
+    answer3: 'Three',
+    answer4: 'Four',
+    rightAnswer: 'two'
+  },
+  {
+    question: 'How many days are in a year?',
+    answer: '365',
+    answer2: '355',
+    answer3: '345',
+    answer4: '367',
+    rightAnswer: '365'
+  },
+  {
+    question: 'How many sides does a triangle have?',
+    answer: 'Three',
+    answer2: 'Four',
+    answer3: 'Five',
+    answer4: 'Six',
+    rightAnswer: 'three'
+  },
+  {
+    question: 'How many colors are in a rainbow?',
+    answer: 'Five',
+    answer2: 'Six',
+    answer3: 'Seven',
+    answer4: 'Eight',
+    rightAnswer: 'seven'
+  },
+  {
+    question: 'Which country is home to the kangaroo?',
+    answer: 'Australia',
+    answer2: 'Africa',
+    answer3: 'America',
+    answer4: 'Asia',
+    rightAnswer: 'australia'
+  },
+  {
+    question: 'What food do pandas eat?',
+    answer: 'Warms',
+    answer2: 'Fish',
+    answer3: 'Bamboo',
+    answer4: 'Insects',
+    rightAnswer: 'bamboo'
+  },
+  {
+    question: 'How many bones do sharks have?',
+    answer: 'Zero',
+    answer2: 'Twenty-five',
+    answer3: 'Fifty-two',
+    answer4: 'Twenty',
+    rightAnswer: 'zero'
+  },
+  {
+    question: 'Can you name the closest star to Earth?',
+    answer: 'Mercury',
+    answer2: 'The sun',
+    answer3: 'Mars',
+    answer4: 'Venus',
+    rightAnswer: 'the sun'
+  },
+  {
+    question: 'What is the largest continent?',
+    answer: 'Asia',
+    answer2: 'Africa',
+    answer3: 'North America',
+    answer4: 'Australia',
+    rightAnswer: 'asia'
+  },
+  {
+    question: 'How many sides does an Octagon have?',
+    answer: 'Five',
+    answer2: 'Six',
+    answer3: 'Seven',
+    answer4: 'Eight',
+    rightAnswer: 'eight'
+  },
+  {
+    question: 'How many days in a week?',
+    answer: '5',
+    answer2: '6',
+    answer3: '7',
+    answer4: '8',
+    rightAnswer: '7'
+  },
+  {
+    question: 'Which direction does the Sun rise from?',
+    answer: 'North',
+    answer2: 'South',
+    answer3: 'West',
+    answer4: 'East',
+    rightAnswer: 'east'
+  },
+  {
+    question: 'Who is the king of Greek gods?',
+    answer: 'Poseidon',
+    answer2: 'Zeus',
+    answer3: 'Hermes',
+    answer4: 'Demeter',
+    rightAnswer: 'zeus'
+  },
+  {
+    question: 'What is the capital of canada?',
+    answer: 'Vancouver',
+    answer2: 'Toronto',
+    answer3: 'Calgary',
+    answer4: 'Ottawa',
+    rightAnswer: 'ottawa'
+  }
+];
